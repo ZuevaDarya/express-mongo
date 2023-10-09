c733cc9c0d793df4c0712d0978ba8bfc2ce86221
ZuevaDarya <zuevadasha55@gmail.com>
2023-10-09 15:51:24 +0300

    update files

---
+++ app.js
@@ -0,0 +1,74 @@
+export default function appSrc(express, bodyParser, createReadStream, crypto, http, m, User) {
+  const app = express();
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+    next();
+  });
+
+  app.get('/login/', (req, res) => {
+    res.send('zdarya');
+  });
+
+  app.post('/insert/', async (req, res) => {
+    const login = req.query.login || req.body.login;
+    const password = req.query.password || req.body.password;
+    const URL = req.query.URL || req.body.URL;
+    const newUser = new User({ login, password });
+
+    try {
+      await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true});
+    } catch (e) {
+      console.log(e.codeName);
+    }
+
+    try {
+      await newUser.save();
+      
+      res.status(201).json({'Добавлено': login});
+    } catch (e){
+      res.status(400).json({'Ошибка': 'нет пароля'});
+    }
+  });
+
+  app.get('/code/', (req, res) => {
+    const filePath = import.meta.url.substring(7);
+    createReadStream(filePath).pipe(res);
+  });
+
+  app.get('/sha1/:input/', (req, res) => {
+    const input = req.params.input;
+    const sha1Hash = crypto.createHash('sha1').update(input).digest('hex');
+    res.send(sha1Hash);
+  });
+
+  app.all('/req/', (req, res) => {
+    const addr = req.query.addr || req.body.addr;
+    if (!addr) {
+      return res.send('Specify the "addr" parameter');
+    }
+
+    http.get(addr, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        res.send(data);
+      });
+    }).on('error', (err) => {
+      res.send('Error: ' + err.message);
+    });
+  });
+
+  app.all('*', (req, res) => {
+    res.send('zdarya');
+  });
+
+  return app;
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createReadStream } from 'fs';
+import crypto from 'crypto';
+import http from 'http';
+import m from 'mongoose';
+
+import UserModel from './User.js';
+import appSrc from './app.js';
+
+const User = UserModel(m);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, m, User);
+
+app.listen(process.env.PORT)
